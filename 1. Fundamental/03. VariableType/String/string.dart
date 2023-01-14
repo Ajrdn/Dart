9587a16f485732749759efc57164c33ed7beb13d
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-01-14 16:18:34 +1100

    string

---
+++ 1. Fundamental/03. VariableType/String/string.dart
@@ -0,0 +1,11 @@
+void main() {
+  String str1 = 'String 1';
+  String str2 = 'String 2';
+
+  print(str1);
+  print(str2);
+  print(str1 + str2);
+  print(str1 + ' ' + str2);
+  print('${str1} ${str2}');
+  print('$str1 $str2');
+}
