0066bec70470b6ff896351164244f7c5de9df399
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-01-23 09:25:10 +0900

    map5

---
+++ 1. Fundamental/08. Map/map5.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, bool> isHarryPotter = {
+    'Harry Potter' : true,
+    'Ron Weasley' : true,
+    'Hermione Granger' : true,
+  };
+  
+  print(isHarryPotter);
+
+  print(isHarryPotter.keys);
+
+  print(isHarryPotter.values);
+}
