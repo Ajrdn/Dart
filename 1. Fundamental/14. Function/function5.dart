ea205bebd0144895f10c0de42e621921fa207fb9
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-02-01 09:07:08 +0900

    function5

---
+++ 1. Fundamental/14. Function/function5.dart
@@ -0,0 +1,23 @@
+void main() {
+  addNumbers(num2: 20, num1: 10, num3: 30);
+  addNumbers(num1: 10, num2: 30, num3: 40);
+}
+
+addNumbers({
+  required int num1,
+  required int num2,
+  required int num3,
+}) {
+  int sum = num1 + num2 + num3;
+
+  print('num1 : $num1');
+  print('num2 : $num2');
+  print('num3 : $num3');
+  print('sum : $sum');
+
+  if(sum % 2 == 0) {
+    print('sum은 짝수입니다.');
+  } else {
+    print('sum은 홀수입니다.');
+  }
+}
