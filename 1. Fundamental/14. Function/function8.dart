d5b5736f4c24429b1e5474d35cdd9f9475d502d3
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-02-02 09:46:11 +0900

    function8

---
+++ 1. Fundamental/14. Function/function8.dart
@@ -0,0 +1,27 @@
+void main() {
+  int result1 = addNumbers(10, num2: 20, num3: 30);
+
+  int result2 = addNumbers(10, num2: 30);
+
+  print('result1 + result2 : ${result1 + result2}');
+}
+
+int addNumbers(int num1, {
+  required int num2,
+  int num3 = 30,
+}) {
+  int sum = num1 + num2 + num3;
+
+  print('num1 : $num1');
+  print('num2 : $num2');
+  print('num3 : $num3');
+  print('sum : $sum');
+
+  if(sum % 2 == 0) {
+    print('sum은 짝수입니다.');
+  } else {
+    print('sum은 홀수입니다.');
+  }
+
+  return sum;
+}
