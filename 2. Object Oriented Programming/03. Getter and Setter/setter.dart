26a1c13e73e388c1e09c41ea03ee4a0f9b0f67e5
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-02-11 20:09:51 +0900

    setter

---
+++ 2. Object Oriented Programming/03. Getter and Setter/setter.dart
@@ -0,0 +1,46 @@
+class Idol {
+  String name;
+  List<String> members;
+
+  Idol(this.name, this.members);
+
+  Idol.fromList(List values)
+  : this.members = values[0],
+    this.name = values[1];
+
+  void sayHello() {
+    print('안녕하세요. ${this.name}입니다.');
+  }
+
+  void introduce() {
+    print('저희 멤버는 ${this.members}가 있습니다.');
+  }
+
+  String get firstMember {
+    return this.members[0];
+  }
+  
+  set firstMember(String name) {
+    this.members[0] = name;
+  }
+}
+
+void main() {
+  Idol blackPink = Idol(
+    '블랙핑크',
+    ['지수', '제니', '리사', '로제']
+  );
+  
+  Idol bts = Idol.fromList(
+    [
+      ['RM', '진', '슈가', '제이홉', '지민', '뷔', '정국'],
+      'BTS'
+    ]
+  );
+
+  blackPink.firstMember = '수지';
+  bts.firstMember = 'MR';
+
+  print(blackPink.firstMember);
+  print(bts.firstMember);
+}
