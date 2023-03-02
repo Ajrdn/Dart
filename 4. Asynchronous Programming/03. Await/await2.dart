97fd64c4b2d319ef2b38646136fe96b364491715
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-03-03 07:59:38 +0900

    await2

---
+++ 4. Asynchronous Programming/03. Await/await2.dart
@@ -0,0 +1,14 @@
+void main() {
+  addNumbers(1, 1);
+  addNumbers(2, 2);
+}
+
+void addNumbers(int number1, int number2) async {
+  print('$number1 + $number2 계산 중');
+
+  await Future.delayed(Duration(seconds: 2), () {
+    print('$number1 + $number2 = ${number1 + number2} 계산 완료');
+  });
+
+  print('함수 완료');
+}
